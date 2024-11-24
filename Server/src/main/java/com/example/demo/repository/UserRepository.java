7aa1e12a2981d448ceb115c63e4ac1d6695bcb4d
Polina <pglinyanaya@mail.ru>
2024-11-24 19:19:16 +0300

    UpdateStart

--- Server/src/main/java/com/example/demo/repository/UserRepository.java
+++ Server/src/main/java/com/example/demo/repository/UserRepository.java
@@ -11,7 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByUsername(String username);
 
-    Optional<User> findUserByEmail(String email);
-
     Optional<User> findUserById(Long id);
 }
